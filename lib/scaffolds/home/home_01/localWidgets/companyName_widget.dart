4d5f1847e80627db24911df14489b48f39e34be7
Ikeda <ruizuikeda@gmail.com>
2021-04-03 14:55:45 -0300

    teste de TODO

--- lib/scaffolds/home/home_01/localWidgets/companyName_widget.dart
+++ lib/scaffolds/home/home_01/localWidgets/companyName_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// TODO: Tornar título dinâmico
+// BODY: deve aceitar imagens ou textos
+
 class CompanyNameWidget extends StatelessWidget {
   const CompanyNameWidget({
     Key? key,
